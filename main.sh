23584c4a08cd93dda80787c166204c756fd2348c
Paulo Jannig <paulojannig@proton.me>
2023-11-27 16:31:50 +0100

    update main.sh

--- main.sh
+++ main.sh
@@ -18,7 +18,7 @@ source config.sh
 ./scripts/03.merge_fastq.sh
 nohup ./scripts/04.fastqc.sh >> ${DATASET}/logs/log.01.fastqc.txt
 nohup ./scripts/05.trimgalore.sh >> ${DATASET}/logs/log.02.trimgalore.txt
-#nohup ./scripts/06.star.alignment.sh >> ${DATASET}/logs/log.03.star.alignment.txt
-#nohup ./scripts/07.infer_experiment.sh >> ${DATASET}/logs/log.04.infer_experiment.txt
-#nohup ./scripts/08.featureCounts.sh >> ${DATASET}/logs/log.05.featureCounts.txt
-#nohup ./scripts/09.kallisto.quant.sh >> ${DATASET}/logs/log.06.kallisto.quant.txt
+nohup ./scripts/06.star.alignment.sh >> ${DATASET}/logs/log.03.star.alignment.txt
+nohup ./scripts/07.infer_experiment.sh >> ${DATASET}/logs/log.04.infer_experiment.txt
+nohup ./scripts/08.featureCounts.sh >> ${DATASET}/logs/log.05.featureCounts.txt
+nohup ./scripts/09.kallisto.quant.sh >> ${DATASET}/logs/log.06.kallisto.quant.txt
